008e0ee7feb9382ba6bbb0b8baab2b0c975e5331
Wildboar <trevorgeek@gmail.com>
2024-03-05 15:12:38 +1300

    Add the initial stub for the application

---
+++ Utils/PointGen/PointGen/Engine.h
@@ -0,0 +1,32 @@
+//--------------------------------------------------
+// Defines a basic engine for a vanilla C++ project.
+//
+// @author: Wild Boar
+//
+// @date: 2024-03-05
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <NVLib/Logger.h>
+#include <NVLib/Path/PathHelper.h>
+#include <PointGenLib/ArgUtils.h>
+
+namespace NVL_App
+{
+	class Engine
+	{
+	private:
+		NVLib::Parameters * _parameters;
+		NVLib::Logger* _logger;
+		NVLib::PathHelper * _pathHelper;
+	public:
+		Engine(NVLib::Logger* logger, NVLib::Parameters * parameters);
+		~Engine();
+
+		void Run();
+	};
+}
---
+++ Utils/PointGen/PointGen/Source.cpp
@@ -0,0 +1,44 @@
+//--------------------------------------------------
+// Startup code module
+//
+// @author: Wild Boar
+//
+// @date: 2024-03-05
+//--------------------------------------------------
+
+#include "Engine.h"
+
+//--------------------------------------------------
+// Execution entry point
+//--------------------------------------------------
+
+/**
+ * Main Method
+ * @param argc The count of the incomming arguments
+ * @param argv The number of incomming arguments
+ */
+int main(int argc, char ** argv) 
+{
+    auto logger = NVLib::Logger(2);
+    logger.StartApplication();
+
+    try
+    {
+        auto parameters = NVL_App::ArgUtils::Load("PointGen", argc, argv);
+        NVL_App::Engine(&logger, parameters).Run();
+    }
+    catch (runtime_error exception)
+    {
+        logger.Log(1, "Error: %s", exception.what());
+        exit(EXIT_FAILURE);
+    }
+    catch (string exception)
+    {
+        logger.Log(1, "Error: %s", exception.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    logger.StopApplication();
+
+    return EXIT_SUCCESS;
+}
