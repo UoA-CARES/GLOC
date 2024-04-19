ab03687e4c782d37087e9cf8b48503920876fc63
Wildboar <trevorgeek@gmail.com>
2024-04-19 17:18:00 +1200

    Create a project for the point conversion

---
+++ Utils/PointConvert/Source.cpp
@@ -0,0 +1,81 @@
+//--------------------------------------------------
+// Startup code module
+//
+// @author: Wild Boar
+//
+// @date: 2024-04-19
+//--------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+#include <NVLib/Logger.h>
+#include <NVLib/Path/PathHelper.h>
+#include <NVLib/Parameters/Parameters.h>
+
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+#include "ArgReader.h"
+
+//--------------------------------------------------
+// Function Prototypes
+//--------------------------------------------------
+void Run(NVLib::Parameters * parameters);
+
+//--------------------------------------------------
+// Execution Logic
+//--------------------------------------------------
+
+/**
+ * Main entry point into the application
+ * @param parameters The input parameters
+ */
+void Run(NVLib::Parameters * parameters) 
+{
+    if (parameters == nullptr) return; auto logger = NVLib::Logger(1);
+
+    logger.StartApplication();
+
+    // Create a helper for path tracking
+    auto database = NVL_Utils::ArgReader::ReadString(parameters, "database");
+    auto dataset = NVL_Utils::ArgReader::ReadString(parameters, "dataset");
+    auto pathHelper = NVLib::PathHelper(database, dataset);
+
+    logger.StopApplication();
+}
+
+//--------------------------------------------------
+// Entry Point
+//--------------------------------------------------
+
+/**
+ * Main Method
+ * @param argc The count of the incoming arguments
+ * @param argv The number of incoming arguments
+ * @return SUCCESS and FAILURE
+ */
+int main(int argc, char ** argv) 
+{
+    NVLib::Parameters * parameters = nullptr;
+
+    try
+    {
+        parameters = NVL_Utils::ArgReader::GetParameters(argc, argv);
+        Run(parameters);
+    }
+    catch (runtime_error exception)
+    {
+        cerr << "Error: " << exception.what() << endl;
+        exit(EXIT_FAILURE);
+    }
+    catch (string exception)
+    {
+        cerr << "Error: " << exception << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    if (parameters != nullptr) delete parameters;
+
+    return EXIT_SUCCESS;
+}
