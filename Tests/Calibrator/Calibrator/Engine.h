bd9aff77cbce42140ee3330360f496a31d59623c
Wildboar <trevorgeek@gmail.com>
2024-05-28 16:12:02 +1200

    Add a project to hold the calibrator logic

---
+++ Tests/Calibrator/Calibrator/Engine.h
@@ -0,0 +1,32 @@
+//--------------------------------------------------
+// Defines a basic engine for a vanilla C++ project.
+//
+// @author: Wild Boar
+//
+// @date: 2024-05-28
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <NVLib/Logger.h>
+#include <NVLib/Path/PathHelper.h>
+#include <CalibratorLib/ArgUtils.h>
+
+namespace NVL_App
+{
+	class Engine
+	{
+	private:
+		NVLib::Parameters * _parameters;
+		NVLib::Logger* _logger;
+		NVLib::PathHelper * _pathHelper;
+	public:
+		Engine(NVLib::Logger* logger, NVLib::Parameters * parameters);
+		~Engine();
+
+		void Run();
+	};
+}
