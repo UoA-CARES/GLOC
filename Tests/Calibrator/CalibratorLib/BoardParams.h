208c420873a418639a70046eee87fb6721047e58
Wildboar <trevorgeek@gmail.com>
2024-07-30 13:39:29 +1200

    Fix file format

--- Tests/Calibrator/CalibratorLib/BoardParams.h
+++ Tests/Calibrator/CalibratorLib/BoardParams.h
@@ -18,18 +18,18 @@ namespace NVL_App
 {
 	class BoardParams
 	{
-		private:
-			Size _boardSize;
-			int _blockSize;
-			int _markerSize;
-			int _dictionary;
-		public:
-			BoardParams(Size& boardSize, int blockSize, int markerSize, int dictionary) :
-				_boardSize(boardSize), _blockSize(blockSize), _markerSize(markerSize), _dictionary(dictionary) {}
+	private:
+		Size _boardSize;
+		int _blockSize;
+		int _markerSize;
+		int _dictionary;
+	public:
+		BoardParams(Size& boardSize, int blockSize, int markerSize, int dictionary) :
+			_boardSize(boardSize), _blockSize(blockSize), _markerSize(markerSize), _dictionary(dictionary) {}
 
-			inline Size& GetBoardSize() { return _boardSize; }
-			inline int& GetBlockSize() { return _blockSize; }
-			inline int& GetMarkerSize() { return _markerSize; }
-			inline int& GetDictionary() { return _dictionary; }
+		inline Size& GetBoardSize() { return _boardSize; }
+		inline int& GetBlockSize() { return _blockSize; }
+		inline int& GetMarkerSize() { return _markerSize; }
+		inline int& GetDictionary() { return _dictionary; }
 	};
 }
